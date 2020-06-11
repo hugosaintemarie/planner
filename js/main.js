e704cd966fd87ae0fb3a1ec9e8ca38811a4d90b2
ashugeo <hugo.saintemarie@gmail.com>
2020-06-12 00:48:00 +0200

    Pick start and end

--- js/main.js
+++ js/main.js
@@ -2,37 +2,38 @@ $(document).ready(() => {
     buildCalendar();
 });
 
+$(document).on('change', '#start, #end', () => {
+    buildCalendar();
+});
+
 function buildCalendar() {
-    const today = new Date();
-    // const today = new Date('2020-04-01');
-    const year = today.getFullYear();
-    const month = today.getMonth();
+    const start = new Date(new Date($('#start').val()).setHours(0));
+    const end = new Date(new Date($('#end').val()).setHours(0));
 
-    const firstDayOfMonth = new Date(year, month, 1);
-    const lastDayOfMonth = new Date(year, month + 1, 0);
+    if (end < start) return;
 
-    // console.log(firstDayOfMonth);
-    // console.log(firstDayOfMonth.getDay());
+    // Find first day (first Monday)
+    const firstDay = new Date(new Date(start).setDate(start.getDate() - start.getDay() + (start.getDay() === 0 ? -6 : 1)));
 
+    const days = [firstDay];
+    
+    // Build array of all days from firstDay to end
+    while (days[days.length - 1] < end) days.push(new Date(new Date(days[days.length - 1].valueOf()).setDate(days[days.length - 1].getDate() + 1)));
+
+    // Fill last week
+    while (days.length % 7 !== 0) days.push(new Date(new Date(days[days.length - 1].valueOf()).setDate(days[days.length - 1].getDate() + 1)));
+
+    // Build HTML
     let html = '<div>';
+    for (day of days) {
+        day.setHours(0);
+        
+        const classname = day < start || day > end ? 'out' : '';
 
-    for (let i = 1; i < firstDayOfMonth.getDay(); i += 1) {
-        const day = new Date(year, month - 1, i - firstDayOfMonth.getDay());
-        html += `<div class="previous-month-day">${day.getDate()}</div>`;
-    }
-
-    for (let i = firstDayOfMonth.getDate(); i <= lastDayOfMonth.getDate(); i += 1) {
-        const day = new Date(year, month, i);
-        html += `<div>${i}</div>`;
+        html += `<div class="${classname}">${day.getDate()} ${day.toLocaleDateString('en-US', { month: 'short', year: 'numeric' })}</div>`;
 
         if (day.getDay() === 0) html += '</div><div>';
     }
-
-    for (let i = lastDayOfMonth.getDay(); i < 7; i += 1) {
-        const day = new Date(year, month + 1, i - lastDayOfMonth.getDay() + 1);
-        html += `<div class="next-month-day">${day.getDate()}</div>`;
-    }
-
     html += '</div>';
 
     $('.month-wrap').html(html);
