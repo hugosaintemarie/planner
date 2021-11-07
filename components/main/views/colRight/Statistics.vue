0c1f367094dba024943253449d3a6df74f4696c0
ashugeo <hugo.saintemarie@gmail.com>
2021-11-07 01:27:03 +0100

    Count events by category in stats

--- components/main/views/colRight/Statistics.vue
+++ components/main/views/colRight/Statistics.vue
@@ -6,14 +6,17 @@
             <div
                 v-for="category in categories"
                 :key="category.id"
-                class="flex items-center space-x-3"
+                class="flex items-center"
             >
                 <div
                     class="w-1 h-8 rounded"
                     :style="`background-color: ${category.color}`"
                 ></div>
-                <span class="text-gray-400">{{
+                <span class="ml-3 text-gray-400">{{
                     category.title || '&nbsp;'
+                }}</span>
+                <span class="ml-auto text-gray-400">{{
+                    count(category) || 0
                 }}</span>
             </div>
         </div>
@@ -25,6 +28,13 @@ export default {
     computed: {
         categories() {
             return this.$store.state.categories.list;
+        },
+    },
+    methods: {
+        count(category) {
+            return this.$store.getters['events/onCalendar'].filter(
+                (d) => d.category === category
+            ).length;
         },
     },
 };
