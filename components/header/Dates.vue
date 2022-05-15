68986e952b2708cee48c9821d4795aa3adfc3b1e
Hugo Sainte-Marie <hugo.saintemarie@gmail.com>
2022-05-15 03:41:54 +0200

    Make dates settings reactive on change

--- components/header/Dates.vue
+++ components/header/Dates.vue
@@ -2,18 +2,18 @@
     <div class="flex items-center px-4">
         <input
             id="start"
+            v-model="start"
             type="date"
             name="start"
             class="px-2 py-1 text-gray-500 bg-gray-900 rounded-sm"
-            :value="start"
         />
         <i class="mx-2 text-sm text-gray-600 fas fa-arrow-right"></i>
         <input
             id="end"
+            v-model="end"
             type="date"
             name="end"
             class="px-2 py-1 text-gray-500 bg-gray-900 rounded-sm"
-            :value="end"
         />
     </div>
 </template>
@@ -21,11 +21,27 @@
 <script>
 export default {
     computed: {
-        start() {
-            return this.$store.getters['settings/get']('start');
+        start: {
+            get() {
+                return this.$store.getters['settings/get']('start');
+            },
+            set(value) {
+                return this.$store.dispatch('settings/set', {
+                    key: 'start',
+                    value,
+                });
+            },
         },
-        end() {
-            return this.$store.getters['settings/get']('end');
+        end: {
+            get() {
+                return this.$store.getters['settings/get']('end');
+            },
+            set(value) {
+                return this.$store.dispatch('settings/set', {
+                    key: 'end',
+                    value,
+                });
+            },
         },
     },
 };
--- store/settings.js
+++ store/settings.js
@@ -5,6 +5,18 @@ export const state = () => ({
     },
 });
 
+export const mutations = {
+    set(state, { key, value }) {
+        state.settings[key] = value;
+    },
+};
+
+export const actions = {
+    set({ commit }, { key, value }) {
+        commit('set', { key, value });
+    },
+};
+
 export const getters = {
     get: (state) => (prop) => {
         return state.settings[prop];
